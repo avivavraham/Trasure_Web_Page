461a3730c22d6bf66a1c0294c8c698cd712f1caf
avivavraham <avivavraham@mail.tau.ac.il>
2023-07-18 18:58:30 +0300

    added navigation bar and forms using wtf&flash

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 # create a flask instance
 app = Flask(__name__)
 
@@ -8,6 +10,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+# localhost:5000/user/Aviv
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", user_name=name)
 
 
 # create a custom error page
@@ -22,3 +30,26 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template("500.html")
+
+# Create a name Page
+@app.route('/name', methods=['GET','POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully")
+
+    return render_template("name.html",
+                           name=name,
+                           form=form)
+
+# TODO: modified your secret key for form security
+app.config['SECRET_KEY'] = "my super secret key that no one shuld know"
+
+# create a form class
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
