5f7b7daf3221f0d526df85317c5f97dd4a7d2952
avivavraham <avivavraham@mail.tau.ac.il>
2023-07-19 20:28:25 +0300

    added SQLAlchemy Data Base

--- hello.py
+++ hello.py
@@ -2,20 +2,57 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 # create a flask instance
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+# TODO: modified your secret key for form security
+app.config['SECRET_KEY'] = "my super secret key that no one shuld know"
+# Initialize the Database
+db = SQLAlchemy(app)
+app.app_context().push()
 
 
-# create a route decorator
-@app.route('/')
-def index():
-    return render_template("index.html")
+# create Model
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow())
+
+    # Create a string
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
+
+# create a form class
+class UserForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+class NamerForm(FlaskForm):
+    name = StringField("What's your name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+with app.app_context():
+    db.create_all()
 
 
 # localhost:5000/user/Aviv
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html", user_name=name)
+
+
+# create a route decorator
+@app.route('/')
+def index():
+    return render_template("index.html")
 
 
 # create a custom error page
@@ -31,8 +68,9 @@ def page_not_found(e):
 def page_not_found(e):
     return render_template("500.html")
 
+
 # Create a name Page
-@app.route('/name', methods=['GET','POST'])
+@app.route('/name', methods=['GET', 'POST'])
 def name():
     name = None
     form = NamerForm()
@@ -46,10 +84,24 @@ def name():
                            name=name,
                            form=form)
 
-# TODO: modified your secret key for form security
-app.config['SECRET_KEY'] = "my super secret key that no one shuld know"
 
-# create a form class
-class NamerForm(FlaskForm):
-    name = StringField("What's your name", validators=[DataRequired()])
-    submit = SubmitField("Submit")
+# Create a name Page
+@app.route('/user/add', methods=['GET', 'POST'])
+def add_user():
+    name = None
+    form = UserForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user is None:
+            user = Users(name=form.name.data, email=form.email.data)
+            db.session.add(user)
+            db.session.commit()
+        name = form.name.data
+        form.name.data = ''
+        form.email.data = ''
+        flash("User Added Successfully")
+    our_users = Users.query.order_by(Users.date_added)
+    return render_template("add_user.html",
+                           form=form,
+                           name=name,
+                           our_users=our_users)
