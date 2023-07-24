cd24574b0437b0cca308e23d792911cfb1fae82e
avivavraham <avivavraham@mail.tau.ac.il>
2023-07-24 10:29:22 +0300

    Added Update+Delete btns to the records in the data base

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, flash
+from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, IntegerField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_migrate import Migrate
 
 # create a flask instance
 app = Flask(__name__)
@@ -15,6 +16,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost/our
 app.config['SECRET_KEY'] = "my super secret key that no one should know"
 # Initialize the Database
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 app.app_context().push()
 
 
@@ -23,6 +25,7 @@ class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False, unique=True)
+    age = db.Column(db.Integer)
     date_added = db.Column(db.DateTime, default=datetime.utcnow())
 
     # Create a string
@@ -34,6 +37,7 @@ class Users(db.Model):
 class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
+    age = IntegerField("Age")
     submit = SubmitField("Submit")
 
 
@@ -56,6 +60,36 @@ def user(name):
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+# Update Data Base Record
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    form = UserForm()
+    name_to_update = Users.query.get_or_404(id)
+    if request.method == "POST":
+        name_to_update.name = request.form['name']
+        name_to_update.email = request.form['email']
+        name_to_update.age = request.form['age']
+        try:
+            db.session.commit()
+            flash("User Updated Successfully")
+            return render_template("update.html",
+                                   form=form,
+                                   name_to_update=name_to_update,
+                                   id=id)
+        except:
+            db.session.commit()
+            flash("Error! Looks like there was a problem...\tplease try again. ")
+            return render_template("update.html",
+                                   form=form,
+                                   name_to_update=name_to_update,
+                                   id=id)
+    else:
+        return render_template("update.html",
+                               form=form,
+                               name_to_update=name_to_update,
+                               id=id)
 
 
 # create a custom error page
@@ -96,15 +130,40 @@ def add_user():
     if form.validate_on_submit():
         user = Users.query.filter_by(email=form.email.data).first()
         if user is None:
-            user = Users(name=form.name.data, email=form.email.data)
+            user = Users(name=form.name.data, email=form.email.data, age=form.age.data)
             db.session.add(user)
             db.session.commit()
         name = form.name.data
         form.name.data = ''
         form.email.data = ''
+        form.age.data = 0
         flash("User Added Successfully")
     our_users = Users.query.order_by(Users.date_added)
     return render_template("add_user.html",
                            form=form,
                            name=name,
                            our_users=our_users)
+
+
+# Delete Data Base Record
+@app.route('/delete/<int:id>')
+def delete(id):
+    user_to_delete = Users.query.get_or_404(id)
+    name = None
+    form = UserForm()
+
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("User Deleted Successfully")
+        our_users = Users.query.order_by(Users.date_added)
+        return render_template("add_user.html",
+                               form=form,
+                               name=name,
+                               our_users=our_users)
+    except:
+        flash("Whoops! There was a problem deleting this record...")
+        return render_template("add_user.html",
+                               form=form,
+                               name=name,
+                               our_users=our_users)
